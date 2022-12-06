20aafaf9a145b976fc03c1b78fffaf24e65d73d4
rodcavaco <118564529+rodcavaco@users.noreply.github.com>
2022-12-06 20:54:34 +0000

    aula1

--- ListaArray.java
+++ ListaArray.java
@@ -1,20 +1,53 @@
 public class ListaArray implements EstruturaDeDados{
 
+    private int[] elementos;
+    private int contador;
+
+    public ListaArray () {
+        ListaArray elementos = new ListaArray[1000];
+    
+    }
+
     @Override
-    public boolean insert(int chave) {
-        // TODO Auto-generated method stub
+    public boolean insert (int chave, int[] elementos) {
+        
+        this.contador = 0; 
+        
+        for  (int i =0; i < elementos.lenght; i++) {
+            if (elementos[i] = null) {
+                elementos[i] = chave;
+                this.contador++; 
+                
+                return true;
+            } 
+            
+        }
         return false;
     }
 
     @Override
-    public boolean delete(int chave) {
-        // TODO Auto-generated method stub
+    public boolean delete(int chave, int[] elementos) {
+        for  (int i =0; i < this.contador; i++) {
+            if (chave == elementos[i]) {
+                elementos[i] = elementos[i++];
+
+                this.contador = this.contador - 1;
+
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
-    public boolean search(int chave) {
-        // TODO Auto-generated method stub
+    public boolean search (int chave, int[] elementos) {
+        for (int i =0; i < elementos.lenght; i++) {
+            if (chave == elementos[i]) {
+                
+                return true;
+
+            }
+        }
         return false;
     }
 
@@ -43,6 +76,6 @@ public class ListaArray implements EstruturaDeDados{
     }
 
     public static void main(String[] args) {
-        ListaArray r = new ListaArray();
+        ListaArray r = new ListaArray[1000];
     }
 }
